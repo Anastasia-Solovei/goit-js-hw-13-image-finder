2091e0fb3e61dba7365c515e6605d06ca794c806
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-06-11 23:57:34 +0300

    creates API HTTP request logic

---
+++ src/js/apiService.js
@@ -0,0 +1,35 @@
+const API_KEY = '22025483-02f71c6158d17e06d5b927dc1';
+
+export default class imgsApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchImages() {
+    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+
+    return fetch(url)
+      .then(r => r.json())
+      .then(data => {
+        this.incrementPage();
+        return data.hits;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
